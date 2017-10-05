5ea5892ea70e3afca4373b71ceb81e84f162e2df
Adam Linscott <adam.linscott@probablyrational.com>
2017-10-05 12:11:51 +0100

    2 projects

---
+++ GuessMyNumber/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace GuessMyNumber
+{
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			//create variables
+			int targetNumber = 0;
+			int guess = -1;
+			int noOfGuesses = 0;
+
+			//Greeting message
+			Console.WriteLine("Welcome to the guess my number game \nI am thinking of a number...\n");
+
+			//store random number in public variable
+			targetNumber = ThinkOfNewNumber();
+
+			//run loop until correct number has been guessed
+			while(guess != targetNumber)
+			{
+				Console.WriteLine("Guess number " + (noOfGuesses += 1));
+				Int32.TryParse(Console.ReadLine(), out guess);
+
+				bool guessIsValid = IsGuessValid(guess);
+
+				if (!guessIsValid)
+				{
+					break;
+				}
+
+				IsNumberCorrect(guess, targetNumber);
+			}
+			Console.WriteLine("Thanks for playing!");
+
+			Console.ReadKey();
+		}
+
+		static int ThinkOfNewNumber()
+		{
+			Random rnd = new Random();
+			return rnd.Next(100);
+		}
+
+		static bool IsGuessValid(int myGuess)
+		{
+			if(myGuess > 100)
+			{
+				Console.WriteLine("Guess must be less than or equal to 100!\n");
+				return false;
+			}
+			else if(myGuess < 0)
+			{
+				Console.WriteLine("Guess must be greater than or equal to 0!\n");
+				return false;
+			}
+
+			return true;
+		}
+
+
+		static void IsNumberCorrect(int myGuess, int myTarget)
+		{
+			if(myGuess == myTarget)
+			{
+				Console.WriteLine("Congratulations, you guessed the number correctly\n");
+			}
+			else if(myGuess > myTarget && myGuess <= 100)
+			{
+				Console.WriteLine("Too high! Try again!\n");
+			}
+			else if (myGuess < myTarget && myGuess >= 0)
+			{
+				Console.WriteLine("Too Low! Try again!\n");
+			}
+		}
+	}
+}
---
+++ GuessMyNumber2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace GuessMyNumber
+{
+	class Program
+	{
+		static int targetNumber;
+		static int guess;
+
+		static void Main(string[] args)
+		{
+			//Greeting message
+			Console.WriteLine("Welcome to the guess my number game \nI am thinking of a number...\n");
+
+			//store random number in public variable
+			targetNumber = ThinkOfNewNumber();
+
+			//Main game logic runs in this loop
+			int noOfGuesses = 0;
+			while (guess != targetNumber)
+			{
+				Console.WriteLine("Guess number " + (noOfGuesses += 1));
+				Int32.TryParse(Console.ReadLine(), out guess);
+
+				bool guessIsValid = IsGuessValid();
+
+				if (!guessIsValid)
+				{
+					break;
+				}
+
+				IsNumberCorrect();
+			}
+			Console.WriteLine("Thanks for playing!");
+
+			//This stops the game exiting straight away when run in debug mode
+			Console.ReadKey();
+		}
+
+
+		static int ThinkOfNewNumber()
+		{
+			Random rnd = new Random();
+			return rnd.Next(100);
+		}
+
+
+		static bool IsGuessValid()
+		{
+			if (guess > 100)
+			{
+				Console.WriteLine("Guess must be less than or equal to 100!\n");
+				return false;
+			}
+			else if (guess < 0)
+			{
+				Console.WriteLine("Guess must be greater than or equal to 0!\n");
+				return false;
+			}
+
+			return true;
+		}
+
+
+		static void IsNumberCorrect()
+		{
+			if (guess == targetNumber)
+			{
+				Console.WriteLine("Congratulations, you guessed the number correctly\n");
+			}
+			else if (guess > targetNumber && guess <= 100)
+			{
+				Console.WriteLine("Too high! Try again!\n");
+			}
+			else if (guess < targetNumber && guess >= 0)
+			{
+				Console.WriteLine("Too Low! Try again!\n");
+			}
+		}
+	}
+}
